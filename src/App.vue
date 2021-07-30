bd8ec4cb9d9a553996d7d75fe325359ce22caf75
odipojames <odipojames12@gmai.com>
2021-07-30 18:26:53 +0300

    to do

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+<Header @toggle-add-task='toggleAddTask' title="Task Tracker" :showAddTask="showAddTask" />
+<div v-if="showAddTask">
+<AddTask @add-task='addTask' />
+</div>
+
+<Tasks @delete-task='deleteTask' :tasks="tasks" />
+<router-view></router-view>
+<Footer/>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Header,
+   Tasks,
+   AddTask,
+   Footer
+  },
+  data(){
+    return{
+      tasks:[],
+      showAddTask:false
+    }
+  },
+
+  methods:{
+    async fetchTasks(){
+    const res = fetch('api/tasks');
+    const data =  (await res).json();
+    return data
+    },
+    async fetchTask(id){
+    const res = fetch(`api/tasks${id}`);
+    const data =  (await res).json();
+    return data
+    },
+
+    toggleAddTask(){
+       this.showAddTask = !this.showAddTask
+    },
+    async addTask(task){
+      const res = await fetch('api/tasks',
+      {
+        method:'POST',
+        headers:{
+          'Content-type':'application/json',
+         
+        },
+         body:JSON.stringify(task)
+
+      }
+      )
+      const data =  await res.json()
+
+      this.tasks=[...this.tasks,data]
+    },
+    async deleteTask(id){
+      
+      if(confirm('are you sure you want to delete this?')){
+        const res =  await fetch(`api/tasks/${id}`,
+        {
+          method:'DELETE',
+        })
+        res.status ===200?
+        this.tasks = this.tasks.filter((task)=>task.id !==id):
+        alert('Error occured')
+      }
+    }
+  },
+ async created(){
+   this.tasks = await this.fetchTasks();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  
+  max-width: 500px;
+  margin: 30px auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+  
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>{{title}}
+    <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask? 'close':'Add Task'" :color="showAddTask? 'red':'green' "/>
+    </header>
+</template>
+
+
+<script>
+import Button from './Button.vue'
+export default {
+    name:"Header",
+    props:{
+        title:String,
+        showAddTask:Boolean
+    },
+    components:{
+        Button,
+    }
+}
+</script>
+
+}
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
